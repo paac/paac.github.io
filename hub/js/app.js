e83193f51464dcdce5890b187553bfcd61a66a50
Rob Nixon <robnixon515@gmail.com>
2013-11-14 15:22:18 -0500

    Goddamnit

--- hub/js/app.js
+++ hub/js/app.js
@@ -26,5 +26,4 @@ angular.module('hooptie', ['ngRoute', 'estimator.controller', 'secretary.control
           templateUrl: '/hub/partials/secretary.html'
         }
       );
-      // $locationProvider.html5Mode(true);
     }]);       
