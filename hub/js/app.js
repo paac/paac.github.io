b3e8def1c7956c6cb59fc4fac41de16861cca746
Rob Nixon <robnixon515@gmail.com>
2013-11-15 09:00:57 -0500

    Who knows

--- hub/js/app.js
+++ hub/js/app.js
@@ -1,29 +1,53 @@
 angular.module('hooptie', ['ngRoute', 'estimator.controller', 'estimator.service', 'secretary.controller', 'scheduler.controller', 'secretary.service', 'scheduler.service', 'scheduler.filter', 'firebase', 'ngAnimate', 'ui.date'])
-  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  .config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/hub/', 
-        {	
-          templateUrl: 'partials/dashboard.html',
-        }
-      );
-    $routeProvider
-      .when('/hub/estimator',
-        {	
-          templateUrl: '/hub/partials/estimator.html',
-          controller: 'EstimateCtrl'		
-        }
-      );
-    $routeProvider
-      .when('/hub/scheduler',
-        {	
-          templateUrl: '/hub/partials/scheduler.html',
-          controller: 'ScheduleCtrl'
-        }
-      );
-    $routeProvider
-      .when('/hub/secretary',
+      .when('/',
         {
-          templateUrl: '/hub/partials/secretary.html'
-        }
-      );
-    }]);       
+          templateUrl: 'partials/dashboard.html'
+        })
+      .when('/estimator', 
+        {
+          templateUrl: 'partials/estimator.html',
+          controller: 'EstimateCtrl'
+        })
+      .when('/scheduler',
+        {
+          templateUrl: 'partials/scheduler.html',
+          controller: 'ScheduleCtrl'
+        })
+      .when('/secretary',
+        {
+          templateUrl: 'partials/secretary.html'
+        })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+  // .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  //   $routeProvider
+  //     .when('/hub/', 
+  //       {	
+  //         templateUrl: 'partials/dashboard.html',
+  //       }
+  //     );
+  //   $routeProvider
+  //     .when('/hub/estimator',
+  //       {	
+  //         templateUrl: '/hub/partials/estimator.html',
+  //         controller: 'EstimateCtrl'		
+  //       }
+  //     );
+  //   $routeProvider
+  //     .when('/hub/scheduler',
+  //       {	
+  //         templateUrl: '/hub/partials/scheduler.html',
+  //         controller: 'ScheduleCtrl'
+  //       }
+  //     );
+  //   $routeProvider
+  //     .when('/hub/secretary',
+  //       {
+  //         templateUrl: '/hub/partials/secretary.html'
+  //       }
+  //     );
+  //   }]);       
