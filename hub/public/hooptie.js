739c4205b4e5f0f60d91513535f2377b8e92693b
Rob Nixon <robnixon515@gmail.com>
2013-12-02 11:44:19 -0500

    Removed reference to unused module in app.js

--- hub/public/hooptie.js
+++ hub/public/hooptie.js
@@ -1,4 +1,4 @@
-angular.module('hooptie', ['ngRoute', 'estimator.controller', 'estimator.filter', 'estimator.service', 'secretary.controller', 'scheduler.controller', 'secretary.service', 'scheduler.service', 'scheduler.filter', 'firebase', 'ngAnimate', 'ui.date'])
+angular.module('hooptie', ['ngRoute', 'estimator.controller', 'estimator.service', 'secretary.controller', 'scheduler.controller', 'secretary.service', 'scheduler.service', 'scheduler.filter', 'firebase', 'ngAnimate', 'ui.date'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     //Don't try to do anything smart with this. Angular routes are fucking stupid and this will all break without any hint of an error.
     $routeProvider
