990882940fce59ddb22c831a1423089801dd0d6f
Rob Nixon <rob@princessanneauto.com>
2015-07-07 08:14:00 -0400

    updated hours

---
+++ blah.py
@@ -0,0 +1,10 @@
+    Except with
+        Python you inevitably
+            get stuck with "blocks" that look
+            like this and I dare you
+                to tell me
+                whether each line of this example is
+                    properly indented
+                just by
+                glancing
+                    at it
--- hub/js/app.js
+++ hub/js/app.js
@@ -1,6 +1,5 @@
 angular.module('hooptie', ['ngRoute', 'estimator.controller', 'estimator.service', 'secretary.controller', 'scheduler.controller', 'secretary.service', 'scheduler.service', 'scheduler.filter', 'firebase', 'ngAnimate', 'ui.date', 'ui.bootstrap.buttons'])
   .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    //Don't try to do anything smart with this. Angular routes are fucking stupid and this will all break without any hint of an error.
     $routeProvider
       .when('/hub/',
         {
